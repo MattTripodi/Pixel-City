d804bd3b3011d8eb22428a7288f4b41b03953639
Matt Tripodi <mtripodi1027@gmail.com>
2017-09-20 13:27:05 -0400

    Added collectionView programtically

---
+++ Pixel-City/View/PhotoCell.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoCell.swift
+//  Pixel-City
+//
+//  Created by Matt Tripodi on 9/20/17.
+//  Copyright © 2017 Matthew Tripodi. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+    
+}
